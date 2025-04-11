4a704378b167847ac9b6941cb06c4bf415cab0d7
João Pedro Junqueira <pedrojunq2015@hotmail.com>
2025-04-10 21:44:33 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.botao').forEach(button => {
-    button.addEventListener('click', function() {
-        const emptySlot = document.querySelector('.letter-slot:empty');
-        if (emptySlot) {
-            emptySlot.textContent = this.textContent;
-        }
-    });
-});
